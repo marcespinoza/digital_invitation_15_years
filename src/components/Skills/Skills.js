ae26ff31ceafda9c29d92107687ffcec3c14f981
marcespinoza <you@example.com>
2023-09-25 09:13:10 -0300

    neon button added

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -19,29 +19,11 @@ function Skills() {
     return (
         <div className="skills" style={{backgroundColor: theme.secondary}}>
             <div className="skillsHeader">
-                <h2 style={{color: theme.primary}}>Skills</h2>
+                <h2>Skills</h2>
             </div>
-            <div className="skillsContainer">
-                <div className="skill--scroll">
-                    <Marquee 
-                        gradient={false} 
-                        speed={80} 
-                        pauseOnHover={true}
-                        pauseOnClick={true} 
-                        delay={0}
-                        play={true} 
-                        direction="left"
-                    >
-                        {skillsData.map((skill, id) => (
-                            <div className="skill--box" key={id} style={skillBoxStyle}>
-                                <img src={skillsImage(skill)} alt={skill} />
-                                <h3 style={{color: theme.tertiary}}>
-                                    {skill}
-                                </h3>
-                            </div>
-                        ))}
-                    </Marquee>
-                </div>
+            <div>
+            <button class="neons">Hello</button>
+
             </div>
         </div>
     )
